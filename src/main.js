814b6359a46b9358ac43936c77432c37e28ece10
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-03-10 17:36:58 +0000

    wft express

--- src/main.js
+++ src/main.js
@@ -93,7 +93,7 @@ function ready(e) {'use strict';
       },
       geoOptions
     ),
-    socket = io(location.protocol + '//' + location.hostname + ':8080'),
+    socket = io('ws://' + location.hostname + ':8080'),
     stop = function (e) {
       e.preventDefault();
       e.stopPropagation();
