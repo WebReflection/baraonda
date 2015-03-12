1c458bad6a9bea0b094821e9eaf6a507111475c7
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-03-12 16:26:00 +0000

    trying postgres on heroku

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var
+  PORT = process.env.PORT || 3000,
+  DYNO = process.env.DYNO || 'unnamed.dyno',
+  connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/baraonda'
+  pg = require('pg').native,
+  client = new pg.Client(connectionString),
+  express = require('express'),
+  app = express(),
+  server = require('http').Server(app),
+  io = require('socket.io')(server),
+  utils = require('./src/utils.js')
+;
 
-var utils = require('./src/utils.js');
-
-var PORT = process.env.PORT || 3000;
-var DYNO = process.env.DYNO || 'unnamed.dyno';
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
 
 server.listen(PORT, function(err) {
   if (err) throw err;
